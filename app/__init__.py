5c446d7adc8c5b9769c7494cb4f182985d7fa93c
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-01-12 16:51:35 +0530

    sqlite database setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,13 @@
 from flask import Flask
 from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
 # Here __name__ is predefined variable; which is set to name of the module, which is app here.
 app = Flask(__name__)
 app.config.from_object(Config)
 
+db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
-from app import routes
+from app import routes,models
