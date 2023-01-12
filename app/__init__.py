548ff7cd8e0dfa1aa6ccc4205133ea9e77133cd0
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-01-12 20:56:22 +0530

    Added Login and Register || removed db file from repo

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,14 @@ from flask import Flask
 from app.config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 # Here __name__ is predefined variable; which is set to name of the module, which is app here.
 app = Flask(__name__)
 app.config.from_object(Config)
 
 db = SQLAlchemy(app)
-migrate = Migrate(app,db)
-
-from app import routes,models
+migrate = Migrate(app, db)
+login = LoginManager(app)
+login.login_view = 'login'
+from app import routes, models
