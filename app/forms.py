fc949aa8fd9a56066eba5ede4a5622dd6fa7bf74
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-01-14 20:15:17 +0530

    Added User Profiles and Error Logging

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, EqualTo,ValidationError
+from wtforms import StringField, TextAreaField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from app.models import User
+
 
 class LoginForm(FlaskForm):
     username = StringField('username', validators=[DataRequired()])
@@ -17,14 +18,29 @@ class RegisterationForm(FlaskForm):
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
-    def validate_username(self,username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
 
         if user is not None:
             raise ValidationError('Please use a different username')
 
-    def validate_email(self,email):
+    def validate_email(self, email):
         email = User.query.filter_by(email=email.data).first()
         if email is not None:
             raise ValidationError('Email already used')
 
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About me', validators=[Length(min=0, max=300)])
+    submit = SubmitField('submit')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Please use a different username')
