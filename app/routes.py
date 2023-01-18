f7388c9eb92c260221e362778ada8ac2521c8879
Im-gkira <gurkirat.11912048@iiitsonepat.ac.in>
2023-01-18 21:31:25 +0530

    Added form to create posts

--- app/routes.py
+++ app/routes.py
@@ -1,28 +1,29 @@
 from flask import render_template, flash, redirect, url_for, request
 from werkzeug.urls import url_parse
 from app import app
-from app.forms import LoginForm, RegisterationForm, EditProfileForm, EmptyForm
+from app.forms import LoginForm, RegisterationForm, EditProfileForm, EmptyForm, PostForm
 from flask_login import current_user, login_user, logout_user, login_required
-from app.models import User
+from app.models import User, Post
 from app import db
 from datetime import datetime
 
 
-@app.route('/')
-@app.route('/index')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 @login_required
 def index():
-    posts = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', posts=posts)
+    form = PostForm()
+
+    if form.validate_on_submit():
+        post = Post(body=form.post.data, author=current_user)
+        db.session.add()
+        db.session.commit()
+        flash('Your post is now live!')
+        return redirect(url_for('index'))
+
+    posts = current_user.followed_posts().all()
+    return render_template("index.html", title='Home Page', form=form,
+                           posts=posts)
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -79,6 +80,7 @@ def user(username):
     ]
     form = EmptyForm()
     return render_template('user.html', user=user, posts=posts, form=form)
+
 
 @app.before_request
 def before_request():
